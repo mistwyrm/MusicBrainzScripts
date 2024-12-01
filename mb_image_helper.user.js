50d1b3c09ea2399e2941dd285114e7ba87e7e3c3
carn <joellikesgiraffes@gmail.com>
2024-12-01 15:10:52 -0500

    Add edit note to Artist Image Helper user script

--- mb_image_helper.user.js
+++ mb_image_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MusicBrainz Image Helper
 // @namespace    https://github.com/lazybookwyrm/MusicBrainzScripts
-// @version      2024-12-01
+// @version      2024-12-01.2
 // @description  Quickly add archived image links to an artist's profile.
 // @author       lazybookwyrm
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=musicbrainz.org
@@ -26,6 +26,6 @@
         var imageElement = document.getElementById("artistImageContainer").querySelector("a");
         var imageURL = imageElement.href;
         var mbId = document.getElementById("mbURL").href;
-        imageElement.href = mbId + "/edit?edit-artist.url.0.text=https://web.archive.org/web/0/" + imageURL + "&edit-artist.url.0.link_type_id=173";
+        imageElement.href = mbId + "/edit?edit-artist.url.0.text=https://web.archive.org/web/0/" + imageURL + "&edit-artist.url.0.link_type_id=173&edit-artist.edit_note=Image URL imported from Spotify/SAMBL using lazybookwyrm's Artist Image Helper userscript https://github.com/lazybookwyrm/MusicBrainzScripts";
     });
 })();
